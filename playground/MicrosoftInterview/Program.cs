c22f32d8ed904660ba5389aff02ebeac7854963c
hadywafa <97hadywafa@gmail.com>
2025-07-09 13:45:47 +0300

    save

--- playground/MicrosoftInterview/Program.cs
+++ playground/MicrosoftInterview/Program.cs
@@ -1,9 +1,9 @@
 ﻿using MicrosoftInterview;
 
-// var s = "anagram";
-var s = "aa";
-// var t = "nagaram";
-var t = "bb";
+var s = "anagram";
+// var s = "aa";
+var t = "nagaram";
+// var t = "bb";
 
 var result = Solution.IsAnagram(s, t);
 
