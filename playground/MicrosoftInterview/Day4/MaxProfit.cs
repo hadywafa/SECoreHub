ee487cbe79c9d9105d66a28549508c3f85b4b681
hady <hady@issi.net>
2025-07-10 12:10:38 +0300

    save

---
+++ playground/MicrosoftInterview/Day4/MaxProfit.cs
@@ -0,0 +1,9 @@
+namespace MicrosoftInterview;
+
+public partial class Solution
+{
+    public static int MaxProfit(int[] prices)
+    {
+        return 0;
+    }
+}
--- playground/MicrosoftInterview/Program.cs
+++ playground/MicrosoftInterview/Program.cs
@@ -1,16 +1,7 @@
 ﻿using MicrosoftInterview;
 
-int[] nums = [28,54,7,-70,22,65,-6]
-;
-// int[] nums = [100,1,2,3,100,1,2,3,4];
-// int[] nums = [1,-1,0];
-// int[] nums = [1, 1, 1];
+int[] prices = [7,1,5,3,6,4];
 
-int k = 100;
-// int k = 3;
-// int k = 0;
-// int k = 2;
-
-var result = Solution.SubArraySum(nums, k);
+var result = Solution.MaxProfit(prices);
 
 Console.WriteLine(result);
