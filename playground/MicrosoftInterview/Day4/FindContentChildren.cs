600dfafd82a6bf621e85d3a9bae4c3a1a2dbb4f9
hady <hady@issi.net>
2025-07-10 19:47:25 +0300

    init

--- playground/MicrosoftInterview/Day4/FindContentChildren.cs
+++ playground/MicrosoftInterview/Day4/FindContentChildren.cs
@@ -4,6 +4,33 @@ public partial class Solution
 {
     public static int FindContentChildren(int[] g, int[] s)
     {
-        return 0;
+        // int[] g = [1,2,3];
+        // int[] s = [1,1];
+        //-------------------------
+        Array.Sort(g);
+        Array.Sort(s);
+        //-------------------------
+        int happyChildrenCount = 0;
+        int pointerForUsedCookie = 0;
+        //-------------------------
+        for (int i = 0; i < g.Length; i++)
+        {
+            if (pointerForUsedCookie == s.Length)
+                break;
+
+            var currentChildGreed = g[i];
+            var currentCookieSize = s[pointerForUsedCookie];
+
+            if (currentChildGreed > currentCookieSize)
+                break;
+
+            if (currentChildGreed <= currentCookieSize)
+            {
+                happyChildrenCount++;
+                pointerForUsedCookie++;
+            }
+        }
+
+        return happyChildrenCount;
     }
 }
--- playground/MicrosoftInterview/Program.cs
+++ playground/MicrosoftInterview/Program.cs
@@ -3,6 +3,13 @@
 int[] g = [1,2,3];
 int[] s = [1,1];
 
+//----
+
+// int[] g = [10,9,8,7,10,9,8,7];
+// int[] s = [10,9,8,7];
+
+//----
+
 var result = Solution.FindContentChildren(g,s);
 
 Console.WriteLine(result);
