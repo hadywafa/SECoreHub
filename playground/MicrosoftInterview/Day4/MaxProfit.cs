9b8d7e77d835467ba94f0b962c432f4338483269
hady <hady@issi.net>
2025-07-10 13:55:01 +0300

    greedy

--- playground/MicrosoftInterview/Day4/MaxProfit.cs
+++ playground/MicrosoftInterview/Day4/MaxProfit.cs
@@ -4,6 +4,19 @@ public partial class Solution
 {
     public static int MaxProfit(int[] prices)
     {
-        return 0;
+        //[7,1,5,3,6,4]
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        foreach (var price in prices)
+        {
+            if (price < minPrice)
+                minPrice = price;
+
+            if ((price - minPrice) > maxProfit)
+                maxProfit = price - minPrice;
+        }
+
+        return maxProfit;
     }
 }
--- playground/MicrosoftInterview/Program.cs
+++ playground/MicrosoftInterview/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MicrosoftInterview;
 
 int[] prices = [7,1,5,3,6,4];
+// int[] prices = [7,6,4,3,1];
 
 var result = Solution.MaxProfit(prices);
 
