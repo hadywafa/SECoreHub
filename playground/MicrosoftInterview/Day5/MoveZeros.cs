f36b0e2ce7ccb8725fa592205096819ac443c082
hadywafa <97hadywafa@gmail.com>
2025-07-11 07:58:25 +0300

    finish

--- playground/MicrosoftInterview/Day5/MoveZeros.cs
+++ playground/MicrosoftInterview/Day5/MoveZeros.cs
@@ -6,36 +6,54 @@ public partial class Solution
     {
         //[0,1,0,3,12]
         //[0,0,1]
-        int targetSwap = 0;
-
+        //-------------------------------------
         for (int i = 0; i < nums.Length; i++)
         {
-            if (nums[i] != 0)
+            if (nums[i] == 0)
                 continue;
 
-            for (int j = i; j < nums.Length; j++)
+            for (int j = i; j > 0; j--)
             {
-                if (nums[j] == 0)
-                    continue;
-
-                //swap
-                var origin = nums[i];
                 var current = nums[j];
                 var prev = nums[j - 1];
 
-                if (origin == 0)
-                {
-                    nums[i] = current;
-                    nums[j - 1] = origin;
-                }
-                else
-                {
-                    nums[j] = prev;
-                    nums[j - 1] = current;
-                }
+                if (prev != 0)
+                    break;
+
+                nums[j] = prev;
+                nums[j - 1] = current;
             }
         }
 
         System.Console.WriteLine(nums.HwToString());
     }
 }
+
+
+// {
+//     //[0,1,0,3,12]
+//     //[0,0,1]
+//     //-------------------------------------
+//     int lastZeroIndex = -1;
+//     for (int i = 0; i < nums.Length; i++)
+//     {
+//         if (nums[i] == 0 && (i + 1) < nums.Length)
+//         {
+//             var next = nums[i + 1];
+//             if (next == 0)
+//             {
+//                 lastZeroIndex = i;
+//                 continue;
+//             }
+//             else
+//             {
+//                 lastZeroIndex = i + 1;
+//             }
+
+//             nums[i + 1] = 0;
+//             nums[lastZeroIndex] = next;
+//         }
+//     }
+
+//     System.Console.WriteLine(nums.HwToString());
+// }
--- playground/MicrosoftInterview/Program.cs
+++ playground/MicrosoftInterview/Program.cs
@@ -1,11 +1,6 @@
 ﻿using MicrosoftInterview;
 
- 
-// int[] nums = [0,1,0,3,12]; 
-int[] nums = [0,0,1]; 
+// int[] nums = [0,1,0,3,12];
+int[] nums = [0, 0, 1];
 
-
-
- Solution.MoveZeroes(nums);
-
- 
+Solution.MoveZeroes(nums);
