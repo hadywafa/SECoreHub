c247d9799335ef521f5e156228859f887510025d
hadywafa <97hadywafa@gmail.com>
2025-07-11 12:44:20 +0300

    init last one

---
+++ playground/MicrosoftInterview/Day5/MergeIntervals.cs
@@ -0,0 +1,11 @@
+namespace MicrosoftInterview;
+
+public partial class Solution
+{
+    public static int[][] MergeIntervals(int[][] intervals)
+    {
+        // [[1,3],[2,6],[8,10],[15,18]] => [[1,6],[8,10],[15,18]]
+        int[][] x = { [], [] };
+        return x;
+    }
+}
