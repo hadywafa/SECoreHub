989cdcf7f0a0f52539a99ce6b40d671859f7df07
hady <hady@issi.net>
2025-07-13 17:07:06 +0300

    save

--- playground/NeetCode/6.linked-list/3.Linked List Cycle.cs
+++ playground/NeetCode/6.linked-list/3.Linked List Cycle.cs
@@ -4,19 +4,43 @@ public static class P3
 {
     public static void Run()
     {
+        var node1 = new ListNode(3);
+        var node2 = new ListNode(2);
+        var node3 = new ListNode(0);
+        var node4 = new ListNode(-4);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node2; // cycle here
         //------------------------------------------
-        int[] arrHead = [3, 2, 0, -4];
 
-        // int[] values = [];
-        ListNode head = ListNode.CreateLinkedList(arrHead);
-
-        var result = HasCycle_S1(head);
+        var result = HasCycle_S1(node1);
         Console.WriteLine(result);
     }
 
     public static bool HasCycle_S1(ListNode head)
     {
-        return true;
+        var bag = new HashSet<ListNode>();
+        int pos = -1;
+        while (true)
+        {
+            if (head == null)
+            {
+                System.Console.WriteLine(pos);
+                return false;
+            }
+            else if (bag.Contains(head))
+            {
+                System.Console.WriteLine(pos);
+                return true;
+            }
+            else
+            {
+                pos++;
+                bag.Add(head);
+                head = head.next;
+            }
+        }
     }
-    
 }
