02ee901f7fb7949f32c0402f5030994fe2f655a3
hady <hady@issi.net>
2025-07-19 16:39:27 +0300

    fx

---
+++ playground/NeetCode/4.stack/3.Evaluate Reverse Polish Notation.cs
@@ -0,0 +1,17 @@
+namespace NeetCode.Stack;
+
+public class P3
+{
+    public static void Run()
+    {
+        string[] tokens = ["2", "1", "+", "3", "*"];
+        var result = EvalRPN(tokens);
+        System.Console.WriteLine(result);
+    }
+
+    public static int EvalRPN(string[] tokens)
+    {
+        return 0;
+    }
+
+}
