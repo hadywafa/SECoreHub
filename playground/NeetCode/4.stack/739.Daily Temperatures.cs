e1558b6585379a1d6c4e1cea06bb742b6ff5a1fa
hadywafa <97hadywafa@gmail.com>
2025-08-02 14:30:31 +0300

    save

--- playground/NeetCode/4.stack/739.Daily Temperatures.cs
+++ playground/NeetCode/4.stack/739.Daily Temperatures.cs
@@ -5,12 +5,31 @@ public class P739
     public static void Run()
     {
         int[] temperatures = [73, 74, 75, 71, 69, 72, 76, 73];
-        var result = DailyTemperatures(temperatures);
+        var result = DailyTemperatures_1(temperatures);
         System.Console.WriteLine(result.ToArray().HwToString());
     }
 
+    public static int[] DailyTemperatures_1(int[] temperatures)
+    {
+        int[] answers = new int[temperatures.Length];
+        var prevColdTempIndecies = new Stack<int>();
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            while (
+                prevColdTempIndecies.Count > 0
+                && temperatures[i] > temperatures[prevColdTempIndecies.Peek()]
+            )
+            {
+                int prevColdTempIndex = prevColdTempIndecies.Pop();
+                answers[prevColdTempIndex] = i - prevColdTempIndex;
+            }
+            prevColdTempIndecies.Push(i);
+        }
+        return answers;
+    }
+
     // ❌⌛ TIME_OUT
-    public static int[] DailyTemperatures(int[] temperatures)
+    public static int[] DailyTemperatures_2(int[] temperatures)
     {
         var list = new List<int>();
         for (int i = 0; i < temperatures.Length; i++)
